4c2fc39ba5fc22cfacd027168ad83090f2fefbce
farahhaddar <farah.haddar99@gmail.com>
2020-10-11 00:04:23 +0300

    Basic 2 step 5

---
+++ php-basics-02/Step_5.php
@@ -0,0 +1,17 @@
+<?php
+$num = "two";
+
+switch ($num) {
+  case "one":
+    echo "1";
+    break;
+  case "two":
+    echo "2";
+    break;
+  case "three":
+    echo "3";
+    break;
+  default:
+    echo "thats not a number";
+}
+?>
