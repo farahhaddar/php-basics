5bf76413ee404f99d1408fe86b9429aeee6197e2
farahhaddar <farah.haddar99@gmail.com>
2020-10-10 14:50:29 +0300

    Basic 1 step 3

---
+++ php-basics-01/Step_3.php
@@ -0,0 +1,9 @@
+<?php
+$name= "farah haddar";
+$email= "farah.haddar99@gmail.com";
+echo " My nam eis $name and my email is $email";
+$height= 5;
+$width= 10;
+$area= $height*$width;
+echo " the area is $area";
+?>
