901d904b912ce1e8c36c0a813185e968b650bb87
farahhaddar <farah.haddar99@gmail.com>
2020-10-10 15:38:57 +0300

    Basic 1 step 4

---
+++ php-basics-01/Step_4.php
@@ -0,0 +1,8 @@
+<?php
+$name= "farah haddar";
+$position= "web developer";
+$github_url="https://github.com/farahhaddar";
+echo "Hello, I'm $name, I'm a $position please check my github link $github_url";
+echo "<br>Hello, I'm". $name." ,I'm a ".$position." please check my github link". $github_url ."<br>";
+echo 'Hello, I\'m '.$name.', I\'m a '.$position. '. Please check my github link ' .$github_url;
+?>
