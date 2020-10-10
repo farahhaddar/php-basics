d6ef6c202d25848840769f05a08beb4575d70b2b
farahhaddar <farah.haddar99@gmail.com>
2020-10-10 16:01:18 +0300

    Basic 1 step 5

---
+++ php-basics-01/Step_5.php
@@ -0,0 +1,12 @@
+<?php 
+$course_name=" data structure";
+$enrolled_students= 24;
+$price=40.5;
+$on_discount= false;
+echo "Course title: $course_name <br> Enrolled Students: $enrolled_students <br>Course price: $price$ <br>Course on discount: ";
+if ( $on_discount == true )
+echo 'Yes';
+else
+echo 'No';
+
+?>
