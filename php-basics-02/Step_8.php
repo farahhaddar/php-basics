235fa4e75d47478332d940fabaa844b848a246ac
farahhaddar <farah.haddar99@gmail.com>
2020-10-11 16:12:50 +0300

    Basic 2 step 8

---
+++ php-basics-02/Step_8.php
@@ -0,0 +1,17 @@
+<?php
+$num=5;
+if ($num >0){
+    if($num%2==0){
+        echo "$num is a power of 2";
+    }else{
+        echo "$num is not a power of 2";
+    }
+
+}else{
+    echo "the number is negative";
+}
+
+
+
+
+?>
