f82fc904a6db4418c12ddeadd79f3d3aa61a5e4f
farahhaddar <farah.haddar99@gmail.com>
2020-10-11 15:02:55 +0300

    Basic 2 step 7

---
+++ php-basics-02/Step_7.php
@@ -0,0 +1,15 @@
+        <?php 
+        $txt=" the most popular programming languages are python and php ";
+        
+            $newTxt = array ("python"=>"php", "php"=>"python");
+            echo strtr($txt,$newTxt);
+        
+
+
+
+
+
+
+
+
+        ?>
